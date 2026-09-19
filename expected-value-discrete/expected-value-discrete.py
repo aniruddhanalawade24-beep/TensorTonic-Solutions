760b3415a438d58486a1fb8fcc1adf996533519e
Aniruddha Nalawade <aniruddha.nalawade24@pccoepune.org>
2026-09-19 12:38:19 +0530

    Solve expected-value-discrete

---
+++ expected-value-discrete/expected-value-discrete.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def expected_value_discrete(x: list, p: list) -> float:
+    """
+    Returns the expected value as a Python float.
+    """
+    # Write code here
+    x = float(np.sum(np.dot(x,p)))
+    return x
